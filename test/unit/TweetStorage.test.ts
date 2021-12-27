f763bcafccfde2a4ea059c9e58e246a2b0e13d6d
Mel Grosser <m.grosser@curasoft.de>
2021-12-27 13:49:45 +0100

    unit test overhaul

--- test/unit/TweetStorage.test.ts
+++ test/unit/TweetStorage.test.ts
@@ -1,22 +1,33 @@
-import { ethers } from "hardhat";
-import { assert, expect } from "chai";
+import { ethers, waffle } from "hardhat";
+import { expect } from "chai";
 import { TweetStorage__factory, TweetStorage } from '../../typechain-types'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+
+const { deployContract } = waffle
 
 describe('TweetStorage Unit Test', () => {
 
+  let sender: SignerWithAddress
+  let controller: SignerWithAddress
+  let account2: SignerWithAddress
   let tweetstorage: TweetStorage
+  const userId = 1;
 
   beforeEach(async () => {
+    [sender, controller, account2] = await ethers.getSigners()
     const TweetStorage = await ethers.getContractFactory('TweetStorage') as TweetStorage__factory
     tweetstorage = await TweetStorage.deploy()
     await tweetstorage.deployed()
+    await tweetstorage.setControllerAddr(controller.address)
   })
 
-  it('can create a tweet and returns the id', async () => {
+  it('should create tweet with ID 1', async () => {
     const expectedTweetId = 1
-    const userId = 1;
-
-    const firstTweetId = await tweetstorage.callStatic.createTweet(userId, "Hello World!")
+    const firstTweetId = await tweetstorage.connect(controller.address).callStatic.createTweet(userId, "Hello World!")
     expect(firstTweetId).to.be.equal(expectedTweetId)
+  })
+
+  it('should revert', async () => {
+    expect(tweetstorage.createTweet(userId, "Hello World!")).to.be.reverted
   })
 })
--- test/unit/UserStorage.test.ts
+++ test/unit/UserStorage.test.ts
@@ -1,22 +1,31 @@
 import { ethers } from "hardhat";
 import { assert, expect } from "chai";
 import { UserStorage__factory, UserStorage } from '../../typechain-types'
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 
 describe('UserStorage Unit Test', () => {
 
+  let sender: SignerWithAddress
+  let controller: SignerWithAddress
+  let account2: SignerWithAddress
   let userstorage: UserStorage
+  const username = ethers.utils.formatBytes32String('tristan')
 
   beforeEach(async () => {
+    [sender, controller, account2] = await ethers.getSigners()
     const UserStorage = await ethers.getContractFactory('UserStorage') as UserStorage__factory
     userstorage = await UserStorage.deploy()
     await userstorage.deployed()
+    await userstorage.setControllerAddr(controller.address)
   })
 
-  it('can create a first user and returns its id of 1', async () => {
+  it('should create tweet with ID 1', async () => {
     const expectedId = 1
-
-    const username = ethers.utils.formatBytes32String('tristan')
-    const firstUserId = await userstorage.callStatic.createUser(username)
+    const firstUserId = await userstorage.connect(controller.address).callStatic.createUser(username)
     expect(firstUserId).to.be.equal(expectedId)
+  })
+
+  it('should revert', async () => {
+    expect(userstorage.createUser(username)).to.be.reverted
   })
 })
