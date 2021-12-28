cadc6babd097a28dd04689dac03cc7209009e6b1
Mel Grosser <m.grosser@curasoft.de>
2021-12-28 13:56:37 +0100

    client setup

---
+++ client/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'DSocial',
+  meta: [
+    { name: 'description', content: 'Decentralized Social Media App' },
+  ],
+})
+</script>
+
+<template>
+  <router-view />
+</template>
---
+++ client/src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Hello world</div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
